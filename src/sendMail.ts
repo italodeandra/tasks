72fb3e54b1e509653cb2424656af4bc3eb1037a1
Ítalo Andrade <italodeandra@gmail.com>
2023-04-23 15:55:27 -0300

    fix app name

--- src/sendMail.ts
+++ src/sendMail.ts
@@ -2,7 +2,7 @@ import prepareSendMail from "@italodeandra/next/mailer/sendMail";
 
 const sendMail = prepareSendMail({
   product: {
-    name: "Ítalo Andrade",
+    name: "Tasks",
     link: "https://italodeandra.de/",
     logo: "https://italodeandra.de/android-chrome-512x512.png",
     copyright: `&copy; ${new Date().getFullYear()} <a href="https://italodeandra.de/" target="_blank">Ítalo Andrade</a>`,
