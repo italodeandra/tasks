745491330016e54925c5969903022cf986f9e8eb
Ítalo Andrade <italodeandra@gmail.com>
2023-09-14 14:51:18 -0300

    fix seconds

--- src/utils/prettyMilliseconds.ts
+++ src/utils/prettyMilliseconds.ts
@@ -1,10 +1,23 @@
 export function prettyMilliseconds(milliseconds: number) {
   let ch = 60 * 60 * 1000;
+  let cm = 60 * 1000;
   let h = Math.floor(milliseconds / ch);
-  let m = Math.round((milliseconds - h * ch) / 60000);
+  let m = Math.floor((milliseconds - h * ch) / cm);
+  let s = Math.round((milliseconds - h * ch - m * cm) / 1000);
+
+  if (s === 60) {
+    m++;
+    s = 0;
+  }
+
   if (m === 60) {
     h++;
     m = 0;
   }
-  return `${h ? `${h}h` : ""}${m ? ` ${m}m` : ""}`;
+
+  const hoursStr = h > 0 ? `${h}h` : "";
+  const minutesStr = m > 0 ? `${m}m` : "";
+  const secondsStr = h === 0 && m === 0 && s > 0 ? `${s}s` : "";
+
+  return `${hoursStr} ${minutesStr}${secondsStr}`.trim();
 }
