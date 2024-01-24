42830461541a714ff872c27df1c49365a6a3115e
Ítalo Andrade <italodeandra@gmail.com>
2024-01-24 15:20:22 -0300

    remove navigation drawer button

--- src/views/layout/Header/Header.tsx
+++ src/views/layout/Header/Header.tsx
@@ -1,25 +1,14 @@
-import { Bars3BottomLeftIcon } from "@heroicons/react/24/solid";
 import ModeToggle from "@italodeandra/ui/components/ModeToggle";
 import Image from "next/image";
 import NextLink from "next/link";
 import UiHeader from "@italodeandra/ui/components/Header";
-import Button from "@italodeandra/ui/components/Button";
-import { navigationDrawerState } from "@italodeandra/ui/components/NavigationDrawer";
 import UserMenu from "../../panel/layout/UserMenu";
 import React from "react";
 import { TimesheetStatus } from "./TimesheetStatus";
 
 export default function Header() {
   return (
-    <UiHeader className="gap-2 px-3.5">
-      <Button
-        icon
-        variant="text"
-        className="-ml-1 mr-1 sm:hidden"
-        onClick={navigationDrawerState.toggle}
-      >
-        <Bars3BottomLeftIcon />
-      </Button>
+    <UiHeader className="gap-2 px-3.5 md:px-3.5">
       <NextLink href="/">
         <Image src="/favicon.ico" width={34} height={34} alt="Logo" />
       </NextLink>
