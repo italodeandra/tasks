8631ba10f579ef7bf8395c0f10085121ab1c892d
Ítalo Andrade <italodeandra@gmail.com>
2024-01-30 08:22:53 -0300

    update lib

--- .yalc/@italodeandra/ui/components/Markdown/Markdown.d.ts
+++ .yalc/@italodeandra/ui/components/Markdown/Markdown.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="react" />
+import { ConverterOptions } from "showdown";
 export type MarkdownProps = {
     children?: string;
     className?: string;
+    options?: ConverterOptions;
 };
-export default function Markdown({ children, className }: MarkdownProps): JSX.Element;
+export default function Markdown({ children, className, options, }: MarkdownProps): JSX.Element;
--- .yalc/@italodeandra/ui/components/Markdown/Markdown.js
+++ .yalc/@italodeandra/ui/components/Markdown/Markdown.js
@@ -4,11 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var jsx_runtime_1 = require("react/jsx-runtime");
-var react_markdown_1 = __importDefault(require("react-markdown"));
-var remark_gfm_1 = __importDefault(require("remark-gfm"));
+var react_1 = require("react");
 var clsx_1 = __importDefault(require("../../utils/clsx"));
+var showdown_1 = __importDefault(require("showdown"));
 function Markdown(_a) {
-    var children = _a.children, className = _a.className;
-    return ((0, jsx_runtime_1.jsx)("div", { className: (0, clsx_1.default)("markdown prose max-w-none dark:prose-invert", className), children: children && ((0, jsx_runtime_1.jsx)(react_markdown_1.default, { remarkPlugins: [remark_gfm_1.default], children: children })) }));
+    var children = _a.children, className = _a.className, options = _a.options;
+    var converter = (0, react_1.useMemo)(function () { return new showdown_1.default.Converter(options); }, 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [JSON.stringify(options)]);
+    return ((0, jsx_runtime_1.jsx)("div", { className: (0, clsx_1.default)("prose prose-zinc dark:prose-invert", className), dangerouslySetInnerHTML: children
+            ? {
+                __html: converter.makeHtml(children),
+            }
+            : undefined }));
 }
 exports.default = Markdown;
